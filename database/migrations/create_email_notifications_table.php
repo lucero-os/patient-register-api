b64a3c13148825e3affdb11cfa93e12eb17c97d6
Osvaldo lucero <osvaldolcuero98@gmail.com>
2023-08-16 21:47:11 -0300

    Add table migrations

---
+++ database/migrations/create_email_notifications_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('email_notifications', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('destinatary_email');
+            $table->timestamps();
+            
+            $table->foreign('notification_id')->references('id')->on('notifications')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
--- database/migrations/create_notification_types_table.php
+++ database/migrations/create_notification_types_table.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('notification_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('notification_type_code');
+            $table->string('notification_type_name');
+            $table->string('notification_type_description');
         });
     }
 
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('notification_types');
     }
 };
